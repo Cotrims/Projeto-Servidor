f7ed5bfee5271b6c57be57a6616dcafceb5b49a3
GIOVANNA PAVANI MARTELLI <u19173@academico.cotuca.unicamp.br>
2019-11-13 11:54:06 -0300

    esquezi

--- Cliente/Cliente.java
+++ Cliente/Cliente.java
@@ -3,7 +3,7 @@ import java.io.*;
 
 public class Cliente
 {
-	public static final String HOST_PADRAO  = "177.220.18.17";
+	public static final String HOST_PADRAO  = "177.220.18.44";
 	public static final int    PORTA_PADRAO = 3000;
 	private String nome;
 
@@ -82,9 +82,11 @@ public class Cliente
         servidor.receba(new PedidoDeNome());
         Nome nome = (Nome)servidor.envie();
         servidor.setNome(nome.getNome());
+	
+	// colocar tudo os negocio bonitinho de par ou impar
 
-		System.out.print("Digite seu nome: ");
-		this.nome = Teclado.getUmString();
+	System.out.print("Digite seu nome: ");
+	this.nome = Teclado.getUmString();
 
         char opcao=' ';
         do
@@ -137,7 +139,6 @@ public class Cliente
 						System.out.print("Aguarde...");
 
 					servidor.receba(new PedidoDeJogo(escolha));
-					servidor.setEscolha(escolha);
 
 					switch(escolha)
 					{
