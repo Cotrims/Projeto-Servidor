7e7b0dcf6bff10c7f0d6870606910562cbae1707
malusperancin <50486625+malusperancin@users.noreply.github.com>
2019-11-14 13:30:53 -0300

    idiota imbecil

--- Servidor/PedidoDeNumero.java
+++ Servidor/PedidoDeNumero.java
@@ -1,3 +1,11 @@
+/**A classe PedidoDeNumero herda de Comunicado e serve para
+indicar ao servidor o pedido de um valor que o cliente colocou no jogo.
+Nela encontramos o construtor e um getter de número.
+@author Giovanna Pavani Martelli.
+@author Maria Luiza Sperancin Mancebo.
+@author Rodrigo Smith Rodrigues.
+@author Vinícius Martins Cotrim.
+@since 2019.*/
 public class PedidoDeNumero extends Comunicado
 {
     private int numeroJogador;
--- Servidor/PedidoDeResultado.java
+++ Servidor/PedidoDeResultado.java
@@ -1,2 +1,10 @@
+/**A classe PedidoDeResultado herda de Comunicado e serve para
+indicar ao servidor um pedido de resultado(quem ganhou o jogo).
+Nela não encontramos nenhum método.
+@author Giovanna Pavani Martelli.
+@author Maria Luiza Sperancin Mancebo.
+@author Rodrigo Smith Rodrigues.
+@author Vinícius Martins Cotrim.
+@since 2019.*/
 public class PedidoDeResultado extends Comunicado
 {}
