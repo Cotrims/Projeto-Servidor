ba8981c008ca33973e5a2a10ee82b04faf6f070b
giovannaPavani <50486607+giovannaPavani@users.noreply.github.com>
2019-11-15 18:36:34 -0300

    docs servidor

--- Servidor/PedidoDeJogo.java
+++ Servidor/PedidoDeJogo.java
@@ -1,14 +1,2 @@
 public class PedidoDeJogo extends Comunicado
-{
-    private char escolha;
-
-    public PedidoDeJogo (char escolha)
-    {
-        this.escolha = escolha;
-    }
-
-    public char getEscolha ()
-    {
-        return this.escolha;
-    }
-}
+{}
