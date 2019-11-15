50357a1f53a0d26756765457189f8ad435417960
malusperancin <50486625+malusperancin@users.noreply.github.com>
2019-11-15 18:54:11 -0300

    CLIENTE TERMINADO CARALHO VAI TOMA NO CU

--- Cliente/PedidoDeNome.java
+++ Cliente/PedidoDeNome.java
@@ -1,14 +1,83 @@
+/**A classe PedidoDeNome herda de Comunicado e serve para
+indicar ao servidor o pedido do nome de um cliente que se conectou.
+Nela encontramos o construtor e um getter de nome.
+@author Giovanna Pavani Martelli.
+@author Maria Luiza Sperancin Mancebo.
+@author Rodrigo Smith Rodrigues.
+@author Vinícius Martins Cotrim.
+@since 2019.*/
 public class PedidoDeNome extends Comunicado
 {
+	/**Armazena o nome do jogador em uma String*/
     private String nome;
 
+	/**
+	Constroi uma nova instância da classe PedidoDeNome.
+	Para tanto, deve ser fornecido uma String que será atribuida à
+	variavel nome.
+	@param nome a String que será atribuida.
+	*/
     public PedidoDeNome(String nome)
     {
         this.nome = nome;
     }
 
+	/**
+	Retorna atributo nome
+	Retorna o atributo nome da instância a qual este método for aplicado.
+	@return a String nome
+	*/
     public String getNome ()
     {
         return this.nome;
     }
+
+	/**
+	 Método que retorna o hash code da instância da classe
+	 Calcula o hashcode do nome representada pela instância a qual o método for aplicado.
+	 @return o hashcode de quem chamou o método
+	 */
+	public int hashCode()
+	{
+		int ret = 1;
+		ret = 3 * ret + new String(this.nome).hashCode();
+		return ret;
+	}
+
+
+    /**
+    Método que retorna se o this é igual ao parametro obj do tipo objeto
+    Verifica se o Object fornecido como parâmetro é o mesmo da instância, resultando true em caso afirmativo,
+    ou false, caso não forem iguais.
+    @param obj do tipo Object é  o objeto com o qual this será comparado
+    @return boolean se this é igual a obj
+    */
+	public boolean equals(Object obj)
+	{
+		if(this == obj)
+			return true;
+
+		if(obj == null)
+			return false;
+
+		if(this.getClass() != obj.getClass())
+			return false;
+
+		PedidoDeNome ped = (PedidoDeNome)obj;
+
+		if(this.nome != ped.nome)
+			return false;
+
+		return true;
+	}
+
+    /**
+    Gera um String com toda a informação presente na classe PedidoDeNome.
+    É feito um String que recebe a String nome.
+    @return um String com o nome do Jogador.
+     */
+	public String toString()
+	{
+		return nome;
+	}
 }
--- Cliente/Resultado.java
+++ Cliente/Resultado.java
@@ -1,14 +1,85 @@
+/**
+A classe Resultado é uma herança de Comunicado que serve para
+passar o atributo vencedor entre o servidor e os clientes, através do
+instância de Resultado.
+Essa classe possui o construtor e um getter.
+@author Giovanna Pavani Martelli.
+@author Maria Luiza Sperancin Mancebo.
+@author Rodrigo Smith Rodrigues.
+@author Vinícius Martins Cotrim.
+@since 2019.
+*/
 public class Resultado extends Comunicado
 {
+	/**Armazena o vencedor em um String*/
     private String vencedor;
 
+	 /**
+	    Constroi uma nova instância da classe Resultado.
+	    Para tanto, deve ser fornecido uma String que será atribuido à
+	    variavel vencedor.
+	    @param vencedor a String que será atribuido.
+    */
     public Resultado (String vencedor)
     {
         this.vencedor = vencedor;
     }
 
+	/**
+		Retorna atributo vencedor
+	    Retorna o atributo vencedor da instância a qual este método for aplicado.
+		@return a String vencedor
+	*/
     public String getVencedor ()
     {
         return this.vencedor;
     }
+
+    /**
+	 Método que retorna o hash code da instância da classe
+	 Calcula o hashcode do vencedor representado pela instância a qual o método for aplicado.
+	 @return o hashcode de quem chamou o método
+	 */
+	public int hashCode()
+	{
+		int ret = 1;
+		ret = 3 * ret + this.vencedor.hashCode();
+		return ret;
+	}
+
+	/**
+	Método que retorna se o this é igual ao parametro obj do tipo objeto
+	Verifica se o Object fornecido como parâmetro é o mesmo da instância, resultando true em caso afirmativo,
+	ou false, caso não forem iguais.
+	@param obj do tipo Object é  o objeto com o qual this será comparado
+	@return boolean se this é igual a obj
+	*/
+	public boolean equals(Object obj)
+	{
+		if(this == obj)
+			return true;
+
+		if(obj == null)
+			return false;
+
+		if(this.getClass() != obj.getClass())
+			return false;
+
+		Resultado res = (Resultado)obj;
+
+		if(this.vencedor != esc.vencedor)
+			return false;
+
+		return true;
+	}
+
+	/**
+	Gera um String com toda a informação presente na classe Escolha.
+	É feito um String que recebe o char escolha.
+	@return um String com a escolha.
+	*/
+	public String toString()
+	{
+		return vencedor;
+	}
 }
